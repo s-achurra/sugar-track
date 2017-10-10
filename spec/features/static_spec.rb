76fb5f4e5f332c8b57bdd83cf78c05ef371295d7
s-achurra <achurra7@gmail.com>
2017-10-10 11:54:04 -0700

    Setup home root path

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'home' do
-    visit root_path
-    expect(page.status_code).to eq(200)
+    it 'can be reached successfully' do
+      visit root_path
+      expect(page.status_code).to eq(200)
+    end
   end
 end
