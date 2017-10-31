388454063a33a090bd0352d0681d2aad0bdc5408
s-achurra <achurra7@gmail.com>
2017-10-31 13:47:46 -0700

    Setup specs for posts and add level presence validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+  validates_presence_of :level
   belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'create' do
+
+    before do
+      @user = FactoryGirl.create(:user)
+      @post = FactoryGirl.create(:post)
+    end
+
+    it 'can be created' do
+      expect(@post).to be_valid
+    end
+
+    it 'requires a level' do
+      @post.level = nil
+      expect(@post).to_not be_valid
+    end
+
+    it 'requires a user' do
+      @post.user = nil
+      expect(@post).to_not be_valid
+    end
+
+    it 'can be created with notes' do
+      @post.notes = 'test notes'
+      expect(@post).to be_valid
+    end
+
+  end
+
 end
