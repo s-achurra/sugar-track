4aede9cf488d91c3bdc3ce4bc4aa81c78fd7e0ca
s-achurra <achurra7@gmail.com>
2017-11-10 14:47:20 -0800

    Include Warden config to allow capybara login_as

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,9 +2,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 require 'spec_helper'
+require 'rspec/rails'
 require 'capybara/rails'
+
+include Warden::Test::Helpers
+Warden.test_mode!
 
 ActiveRecord::Migration.maintain_test_schema!
 
