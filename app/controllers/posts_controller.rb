84f6c3a4ed7af783dbee61f4d322cac71f28ab8b
s-achurra <achurra7@gmail.com>
2017-11-16 14:36:01 -0800

    Setup post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,55 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = current_user.posts
     @levels = current_user.post_data
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      # TODO: decide if this should go to index or show
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:level, :notes)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
